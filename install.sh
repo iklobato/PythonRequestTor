c61d62ed46a9e41199b5ea8868ae047cf5c9a661
iklob <henrique.cadastro@live.com>
2019-09-20 09:18:02 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 apt update && apt -y upgrade && apt-get install -y git net-tools python3 python3-pip tor vim
 pip3 install -r requirements
-git clone https://github.com/henriqueblobato/PythonRequestTor.git
-cd PythonRequestTor
 echo "ControlPort 9051" >> /etc/tor/torrc
 echo HashedControlPassword $(tor --hash-password henrique | cut -d "." -f 2 | tail -n 1) >> /etc/tor/torrc
 echo "CookieAuthentication 1" >> /etc/tor/torrc
